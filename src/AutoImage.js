e007414b1c08f9c0dcaf07e9b6b26c03b20dfe90
Micah Lindley <micahincorporated@gmail.com>
2022-07-20 15:28:42 -0500

    👆 Make images pressable

--- src/AutoImage.js
+++ src/AutoImage.js
@@ -1,26 +1,30 @@
 import * as React from 'react';
-import {Image} from 'react-native';
+import {Image, Linking, Pressable} from 'react-native';
 const AutoImage = ({source, fitWidth = 0}) => {
   const [aspect, setAspect] = React.useState(0);
   return (
-    <Image
-      source={{uri: source}}
-      style={{
-        width: fitWidth,
-        height: fitWidth * aspect,
-        backgroundColor: '#ffffff',
-        borderRadius: 7,
-        marginBottom: 5,
-      }}
-      resizeMode="cover"
-      onLoad={({
-        nativeEvent: {
-          source: {width, height},
-        },
-      }) => {
-        setAspect(height / width);
-      }}
-    />
+    <Pressable
+      onPress={() => Linking.openURL(source)}
+      style={{borderRadius: 7, marginBottom: 5}}
+      useForeground={true}
+      android_ripple={{color: 'rgba(0,0,0,0.2)', foreground: true}}>
+      <Image
+        source={{uri: source}}
+        style={{
+          width: fitWidth,
+          height: fitWidth * aspect,
+          borderRadius: 7,
+        }}
+        resizeMode="cover"
+        onLoad={({
+          nativeEvent: {
+            source: {width, height},
+          },
+        }) => {
+          setAspect(height / width);
+        }}
+      />
+    </Pressable>
   );
 };
 
