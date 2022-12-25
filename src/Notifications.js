6a1c1b1a00d0ffb7ba5316509be921ded9e651f8
Micah Lindley <micahincorporated@gmail.com>
2022-12-25 15:34:20 -0600

    Read and unread notifications

--- src/Notifications.js
+++ src/Notifications.js
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import {View, FlatList} from 'react-native';
-import {Text, useTheme} from 'react-native-paper';
+import {Text, SegmentedButtons, Button, useTheme} from 'react-native-paper';
 import {GlobalContext} from '../App';
 import Notif from './components/Notif';
-import g from '../styles/Global.module.css';
+import s from '../styles/Notifications.module.css';
 
 function Notifications() {
   const {colors} = useTheme();
@@ -11,38 +11,72 @@ function Notifications() {
   const [isLoading, setIsLoading] = React.useState(true);
   const [page, setPage] = React.useState(0);
   const [notifs, setNotifs] = React.useState([]);
+  const [mode, setMode] = React.useState('unread');
+  const [isEnd, setIsEnd] = React.useState(false);
   React.useEffect(() => {
     loadNotifications();
   }, []);
-  const loadNotifications = () => {
+  React.useEffect(() => {
+    setNotifs([]);
+    refresh();
+  }, [mode]);
+  const loadNotifications = n => {
     setIsLoading(true);
-    fetch(`https://api.wasteof.money/messages/unread?page=${page}`, {
+    fetch(`https://api.wasteof.money/messages/${mode}?page=${n ? page : 0}`, {
       headers: {
         Authorization: token,
       },
     })
       .then(res => res.json())
       .then(data => {
-        setNotifs(data.unread);
+        setNotifs(n ? [...n, ...data[mode]] : data[mode]);
         setPage(page + 1);
         setIsLoading(false);
+        setIsEnd(data.last);
       });
   };
   const refresh = () => {
+    setNotifs([]);
     setPage(0);
     loadNotifications();
   };
   const listHeader = (
-    <View
-      style={{
-        marginTop: 20,
-        paddingLeft: 20,
-        marginBottom: 10,
-        marginRight: 0,
-      }}>
-      <Text variant="titleLarge" style={g.header}>
-        Notifications
-      </Text>
+    <View style={s.listHeader}>
+      <SegmentedButtons
+        value={mode}
+        onValueChange={val => {
+          setMode(val);
+        }}
+        buttons={[
+          {
+            value: 'unread',
+            icon: 'email',
+            showSelectedCheck: true,
+            label: 'Unread',
+          },
+          {
+            value: 'read',
+            label: 'Read',
+            icon: 'email-check',
+            showSelectedCheck: true,
+          },
+        ]}
+      />
+    </View>
+  );
+  const listLoading = (
+    <View style={{paddingTop: 20, paddingBottom: 30}}>
+      {!(isLoading && page == 0) && !isEnd && (
+        <Button
+          loading={isLoading}
+          mode="contained-tonal"
+          style={{marginLeft: 'auto', marginRight: 'auto'}}
+          onPress={() => {
+            loadNotifications(notifs);
+          }}>
+          Load more
+        </Button>
+      )}
     </View>
   );
   const renderItem = React.useCallback(({item}) => <Notif notif={item} />, []);
@@ -57,9 +91,10 @@ function Notifications() {
         keyExtractor={item => item._id}
         renderItem={renderItem}
         onRefresh={refresh}
-        ListHeaderComponent={listHeader}
-        refreshing={isLoading}
-        estimatedItemSize={200}
+        ListHeaderComponent={() => listHeader}
+        ListFooterComponent={listLoading}
+        refreshing={isLoading && page == 0}
+        estimatedItemSize={150}
       />
     </View>
   );
