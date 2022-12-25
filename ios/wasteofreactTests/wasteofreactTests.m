98ee53af212ebbe47cef63b45a74697ffcc02bde
Micah Lindley <micahincorporated@gmail.com>
2022-12-25 14:08:54 -0600

    Fix app naming

--- ios/wasteofreactTests/wasteofreactTests.m
+++ ios/wasteofreactTests/wasteofreactTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface wasteofreactTests : XCTestCase
+@interface wasteofmobileTests : XCTestCase
 
 @end
 
-@implementation wasteofreactTests
+@implementation wasteofmobileTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
