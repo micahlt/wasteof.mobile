a5732e377ed1647c36bf9c3ce84904514f31ea76
Micah Lindley <micahincorporated@gmail.com>
2022-12-27 12:27:46 -0600

    Changelog for each update

--- src/components/Changelog.js
+++ src/components/Changelog.js
@@ -1,1 +1,17 @@
-import * as React from 'react';
+import React from 'react';
+
+const Changelog = () => {
+  return (
+    <Dialog visible={visible} onDismiss={hideDialog}>
+      <Dialog.Title>Alert</Dialog.Title>
+      <Dialog.Content>
+        <Paragraph>This is simple dialog</Paragraph>
+      </Dialog.Content>
+      <Dialog.Actions>
+        <Button onPress={hideDialog}>Done</Button>
+      </Dialog.Actions>
+    </Dialog>
+  );
+};
+
+export default Changelog;
